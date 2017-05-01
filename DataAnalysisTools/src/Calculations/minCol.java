cb2a4938da574792a9bc7c6ac23ddba285834142
Tyler Glotz <tyler.glotz@colorado.edu>
2017-04-30 18:30:01 -0600

    Print result of calcs, get rid of print method

--- DataAnalysisTools/src/Calculations/minCol.java
+++ DataAnalysisTools/src/Calculations/minCol.java
@@ -1,17 +1,41 @@
 package Calculations;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import Import.importCSV;
 
 public class minCol implements Calculations {
 
 	@Override
 	public void doCalc(String db, String table,  String un, String pass, String calc, int col){
+		FileWriter filew = new FileWriter(path);
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		try{
+		//connect to database with db (database name), un (MySQL user name), pass (MySQL password)
+		Connection connect = DriverManager.getConnection(String.format("jdbc:mysql://localhost:3306/%s", db), un, pass);
+		String query = String.format("SELECT MIN(%d) as col_min FROM %s", col, table);
+		Statement statem = connect.createStatement();
+		ResultSet res = statem.executeQuery(query);
 		
-	}
-	
-	@Override
-	public void printCalc(String answer){
-		
+		if(res.next()){
+			int min = res.getInt("col_min");
+			System.out.println(min);
+		}
+
+		}catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(minCol.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } 
 	}
 	
 	public static void main(String[] args) {
