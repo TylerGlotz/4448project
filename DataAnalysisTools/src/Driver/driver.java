c1123bba72ab6a08ca6d4518ad9b9534d048b762
Elias Bezanis <eliasbezanis@gmail.com>
2017-04-30 18:32:47 -0600

    updated driver can test imporTXT

--- DataAnalysisTools/src/Driver/driver.java
+++ DataAnalysisTools/src/Driver/driver.java
@@ -16,6 +16,11 @@ public class driver {
 		private String psw;
 		private String name;
 		
+		private static String database="database";
+		private static String table="table";
+		private static String  username="username";
+		private static String password="password";
+		
 	//driver holds important JDBC parameters and represents a working database
 	public driver(String db, String table, String un, String pass){
 		
@@ -26,45 +31,49 @@ public class driver {
 	}
 	
 	//Starts Database
-	public void startDriver(driver d){
-		
-		
-		
-	}
-	
-	public void createDatabase(driver d){
-		//create database
+	public static void startDriver(driver d, String db, String table, String un, String pass){
 		
 	}
 	
 	
 	//Calls correct import to path
 	//If data type has no Delimeter or it is trivial enter "0"
-	public void directImport(String db, String table, String path, String un, String pass, String delimeter){
+	public static void directImport(String db, String table, String path, String un, String pass, String delimeter){
 		
-		/*Example
+		/*
+		 * Sample Call to importTXT(); will use this
+		 * can test import txt
 		Import.importTXT port=new importTXT();
 		port.importFile(db, table, path, un, pass, delimeter);
 		*/
+		
 	}
 	
 	//Calls correct export to path
 	//If data type has no Delimeter or it is trivial enter "0"
-	public void directExport(String db, String table, String path, String un, String pass, String delimeter){
+	public static void directExport(String db, String table, String path, String un, String pass, String delimeter){
 		
 	}
 	
 	//Calls correct calculation
-	public void directCalc(String db, String table, String un, String pass, String calc, int col){
+	public static void directCalc(String db, String table, String un, String pass, String calc, int col){
 		
 	}
 	
 	
 	public static void main(String[] args) {
-
 		
-		//driver d=new driver();
-		//directImport(d.path);
+		//get database running
+		/*
+		 * uncomment for test import text set delimeter and path to appropraite values
+		 * must change private database values
+		driver d=new driver(database,table,username,password);
+		startDriver(d,database,table,username,password);
+		
+		String delimeter="";
+		String path="/sample/read/path";
+		directImport(d.name, d.t, path, d.u, d.psw, delimeter);
+		*/
 		
 	}
 
