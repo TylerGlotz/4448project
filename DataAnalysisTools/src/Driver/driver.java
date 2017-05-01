f4a374f29cf2af49fa6497c6e59b628455513fde
Elias Bezanis <eliasbezanis@gmail.com>
2017-05-01 00:23:49 -0600

    implemented driver methods

--- DataAnalysisTools/src/Driver/driver.java
+++ DataAnalysisTools/src/Driver/driver.java
@@ -1,7 +1,6 @@
 package Driver;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
 import Export.*;
@@ -17,10 +16,10 @@ public class driver {
 		private String psw;
 		private String name;
 		
-		private static String database="dataToolsDB";
-		private static String table="newtable";
-		private static String username="root";
-		private static String password="datatools";
+		private static String database="database";
+		private static String table="table";
+		private static String  username="username";
+		private static String password="password";
 		
 	//driver holds important JDBC parameters and represents a working database
 	public driver(String db, String table, String un, String pass){
@@ -31,27 +30,30 @@ public class driver {
 		pass=psw;
 	}
 	
-	//Starts Database
-	public static void startDriver(driver d, String db, String table, String un, String pass){
-		
-	}
-	
 	
 	//Calls correct import to path
 	//If data type has no Delimeter or it is trivial enter "0"
-	public static void directImport(String db, String table, String path, String un, String pass, String delimeter) throws IOException{
+	public static void directImport(String db, String table, String path, String un, String pass, String delimeter){
 		
+		//this block puts the file extension into a string
+		String extension = "";
+		int i = path.lastIndexOf('.');
+		if (i > 0) {
+		    extension = path.substring(i+1);
+		}
 		
-		String db1="dataToolsDB";
-		String table1="newtable";
-		String un1="root";
-		String pass1="datatools";
+		//if it is a text file
+		if(extension=="txt" || extension=="t"){
+			Import.importTXT port=new importTXT();
+			port.importFile(db, table, path, un, pass, delimeter);
+		}
 		
-		
-		//Import.importTXT port=new importTXT();
-		Import.importCSV port=new importCSV();
-		port.importFile(db1, table1, path, un1, pass1, delimeter);
-		
+		//if it is a CSV file
+			if(extension=="csv" || extension=="c"){
+				Import.importCSV port=new importCSV();
+				port.importFile(db, table, path, un, pass, delimeter);
+			}		
+
 		
 	}
 	
@@ -59,29 +61,62 @@ public class driver {
 	//If data type has no Delimeter or it is trivial enter "0"
 	public static void directExport(String db, String table, String path, String un, String pass, String delimeter){
 		
+		//this block puts the file extension into a string
+		String extension = "";
+		int i = path.lastIndexOf('.');
+		if (i > 0) {
+		    extension = path.substring(i+1);
+		}
+		
+		//if it is a txt file call exportTXT
+		if(extension=="txt" || extension=="t"){
+			Export.exportTXT port=new exportTXT();
+			port.export(db, table, path, un, pass, delimeter);
+		}
+		
+		//if it is a CSV file call exportCSV
+			if(extension=="csv" || extension=="c"){
+				Export.exportCSV port=new exportCSV();
+				port.export(db, table, path, un, pass, delimeter);
+			}
 	}
 	
 	//Calls correct calculation
 	public static void directCalc(String db, String table, String un, String pass, String calc, int col){
 		
+		//calls appropriate column calculation
+		if(calc=="minCol"){
+			minCol calcz=new minCol();
+			calcz.doCalc(db, table, un, pass, calc, col);
+		}
+		if(calc=="maxCol"){
+			maxCol calcz=new maxCol();
+			calcz.doCalc(db, table, un, pass, calc, col);
+		}
+		if(calc=="averageCol"){
+			averageCol calcz=new averageCol();
+			calcz.doCalc(db, table, un, pass, calc, col);
+		}
+		if(calc=="sumCol"){
+			sumCol calcz=new sumCol();
+			calcz.doCalc(db, table, un, pass, calc, col);
+		}
 	}
 	
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
 		//get database running
 		/*
-		uncomment for test import text set delimeter and path to appropraite values
-		must change private database values
-		*/
-		
+		 * uncomment for test import text set delimeter and path to appropraite values
+		 * must change private database values
 		driver d=new driver(database,table,username,password);
 		startDriver(d,database,table,username,password);
 		
-		String delimeter=",";
-		String path="/Users/yolo/GoogleDrive/College Senior Spring/CSCI 4448/4448project/DataAnalysisTools/exampleCSV.csv";
+		String delimeter="";
+		String path="/sample/read/path";
 		directImport(d.name, d.t, path, d.u, d.psw, delimeter);
-		
+		*/
 		
 	}
 
